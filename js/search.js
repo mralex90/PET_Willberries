5722da88895894b6b4423631d2a53428b4be98da
Oleksandr <71518300+mralex90@users.noreply.github.com>
2022-02-24 23:45:43 +0200

    getGoods.js метод fetch

--- js/search.js
+++ js/search.js
@@ -2,7 +2,7 @@ const search = function() {
     const input = document.querySelector('.search-block > input');
     const searchBtn = document.querySelector('.search-block > button');
 
-    input.addEventListener('click', (event) => {
+    searchBtn.addEventListener('click', () => {
         console.log(input.value);
     });
 };
