0642174d06706cb864cbed411e7a15c7d38a511e
Oleksandr <71518300+mralex90@users.noreply.github.com>
2022-02-26 00:02:43 +0200

    отрисовка карточек товара (2)

--- js/getGoods.js
+++ js/getGoods.js
@@ -14,12 +14,12 @@ const getGoods = () => {
 
             goodBlock.innerHTML = `
                 <div class="goods-card">
-                    <span class="label">${good.label}</span>
+                    <span class="label ${good.label ? null : 'd-none'}">${good.label}</span>
                     <img src="db/${good.img}" alt="${good.name}" class="goods-image">
                     <h3 class="goods-title">${good.name}</h3>
                     <p class="goods-description">${good.description}</p>
                     <button class="button goods-card-btn add-to-cart" data-id="${good.id}">
-                        <span class="button-price">${good.price}</span>
+                        <span class="button-price">$${good.price}</span>
                     </button>
                 </div>
             `
