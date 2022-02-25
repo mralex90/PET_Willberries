d3af9b8a833f4d8f0e82144af9c1f9a1c0631a41
Oleksandr <71518300+mralex90@users.noreply.github.com>
2022-02-25 23:18:24 +0200

    метод перебора массива filter

--- js/getGoods.js
+++ js/getGoods.js
@@ -1,30 +1,25 @@
 const getGoods = () => {
     const links = document.querySelectorAll('.navigation-link')
 
-    const getData = () => {
+    const getData = (value, category) => {
         fetch('/db/db.json')
             .then((res) => res.json())
             .then((data) => {
-                console.log(data);
+                const array = category ? data.filter((item) => item[category] === value) : data
+
+                localStorage.setItem('goods', JSON.stringify(array))
             })
     }
 
     links.forEach((link) => {
         link.addEventListener('click', (event) => {
             event.preventDefault()
-            getData()
+            const linkValue = link.textContent;
+            const category = link.dataset.field;
+
+            getData(linkValue, category)
         })
     })
-
-    localStorage.setItem('goods', JSON.stringify([1, 2, 3, 4, 5]));
-
-    const goods = JSON.parse(localStorage.getItem('goods'));
-    console.log(goods);
-
-    console.log(localStorage);
-
-    localStorage.removeItem('goods');
-    console.log(localStorage);
 };
 
 getGoods();
