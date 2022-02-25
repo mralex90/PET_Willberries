a0dbc7d22acbbbfa66766397509a703ab082b30c
Oleksandr <71518300+mralex90@users.noreply.github.com>
2022-02-25 23:58:17 +0200

    отрисовка карточек товара (1)

--- js/getGoods.js
+++ js/getGoods.js
@@ -2,7 +2,33 @@ const getGoods = () => {
     const links = document.querySelectorAll('.navigation-link')
 
     const renderGoods = (goods) => {
-        console.log(goods);
+        const goodsContainer = document.querySelector('.long-goods-list');
+
+        goodsContainer.innerHTML = "";
+
+        goods.forEach(good => {
+            const goodBlock = document.createElement('div')
+
+            goodBlock.classList.add('col-lg-3')
+            goodBlock.classList.add('col-sm-6')
+
+            goodBlock.innerHTML = `
+                <div class="goods-card">
+                    <span class="label">${good.label}</span>
+                    <img src="db/${good.img}" alt="${good.name}" class="goods-image">
+                    <h3 class="goods-title">${good.name}</h3>
+                    <p class="goods-description">${good.description}</p>
+                    <button class="button goods-card-btn add-to-cart" data-id="${good.id}">
+                        <span class="button-price">${good.price}</span>
+                    </button>
+                </div>
+            `
+
+            goodsContainer.append(goodBlock)
+
+            console.log(good);
+        })
+
     }
 
     const getData = (value, category) => {
